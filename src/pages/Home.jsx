290b3b6e73cfd27044a8f077bda09d62ea809e97
Broady <broadybmx@gmail.com>
2024-12-06 20:44:38 -0500

    feat: Start adding modal on Home page , improve css for modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from '@fortawesome/fontawesome-svg-core';
 import { Link } from "react-router-dom";
 import axios from "axios";
 import HomeCinema from "../assets/undraw_home_cinema_l7yl.svg";
-
 
 const Home = () => {
   return (
@@ -21,10 +21,7 @@ const Home = () => {
               </Link>
             </li>
             <li>
-              <Link
-                to="/results"
-                className="nav__link link__hover-effect"
-              >
+              <Link to="/results" className="nav__link link__hover-effect">
                 Find Movies
               </Link>
             </li>
@@ -76,6 +73,49 @@ const Home = () => {
             </div>
           </div>
         </header>
+      </div>
+      <div id="modal" className="modal">
+        <div className="modal__content">
+          <h3 className="modal__title">
+            Do you want your website to look like this one?
+          </h3>
+          <h2 className="modal__sub-title">Please let me know!</h2>
+          <form className="modal__form" onsubmit="contact(event)">
+            <div className="form-item">
+              <label className="form-item__label">Name:</label>
+              <input
+                type="text"
+                className="form__input"
+                name="user_name"
+                required
+              ></input>
+            </div>
+            <div className="form-item">
+              <label className="form-item__label">Email:</label>
+              <input
+                type="email"
+                className="form__input"
+                name="user_email"
+                required
+              ></input>
+            </div>
+            <div className="form-item">
+              <label className="form-item__label">Message:</label>
+              <textarea
+                type="textarea"
+                className="form__textarea"
+                name="message"
+                required
+              ></textarea>
+            </div>
+            <button id="form__submit" className="form__submit">
+              Send it
+            </button>
+          </form>
+        </div>
+        <Link href="#" className="modal__close-button" onclick="toggleModal()">
+          <FontAwesomeIcon icon="times" className="fas fa-times"></FontAwesomeIcon>
+        </Link>
       </div>
     </>
   );
